8e053c5c702a8d96dbbcf0e373f73b8ff8bc4cee
Jeff Wang <wangzhen263@gmail.com>
2018-05-08 14:09:19 +1000

    better message

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# clone fastText
+echo "Cloning fastText..."
 git init
 git remote add -t \* -f origin https://github.com/facebookresearch/fastText.git
 git checkout master
 
-# init virtual env
+echo "Init virtual env..."
 virtualenv .
 
-# install dependencies
+echo "Installing python dependencies..."
 pip install NumPy SciPy pybind11 pandas
 pip install .
 
-# download model files
+echo "Downloading pre-trained english wiki model..."
 curl -O https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.zip
 unzip wiki.en.zip -d wikimodel
